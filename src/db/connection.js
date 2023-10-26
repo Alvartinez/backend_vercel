826dba39734ae28b06da92e2382382771fc0e2c7
Alvartinez <alvaro.martinezudes2@gmail.com>
2023-10-26 16:23:23 -0500

    update connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,14 @@
 const { Sequelize } = require("sequelize");
 const { pg } = require("pg");
 
-const sequelize = new Sequelize(process.env.DB_NAME || "healthTrain", process.env.DB_USER || "postgres", process.env.DB_PASSWORD || "310873292As", {
+// const sequelize = new Sequelize(process.env.DB_NAME || "healthTrain", process.env.DB_USER || "postgres", process.env.DB_PASSWORD || "310873292As", {
+//     host: "localhost",
+//     port: 5432,
+//     dialect: "postgres",
+//     dialectModule: pg
+// });
+
+const sequelize = new Sequelize(process.env.DB_NAME || "osl-udes", process.env.DB_USER || "postgres", process.env.DB_PASSWORD || "Jesus2023*", {
     host: "localhost",
     port: 5432,
     dialect: "postgres",
