054f3656a3c6d0da4edf12be54bb1622799b55c9
Alvartinez <alvaro.martinezudes2@gmail.com>
2023-10-26 16:05:21 -0500

    pg

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require("sequelize");
+const { pg } = require("pg");
 
 const sequelize = new Sequelize(process.env.DB_NAME || "healthTrain", process.env.DB_USER || "postgres", process.env.DB_PASSWORD || "310873292As", {
     host: "localhost",
     port: 5432,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectModule: pg
 });
 
 module.exports = sequelize;
