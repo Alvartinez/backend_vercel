ab253a94bd3a72a382ee03bc8da49b702129ba46
Alvartinez <alvaro.martinezudes2@gmail.com>
2023-10-26 16:09:48 -0500

    update

--- src/models/curso.js
+++ src/models/curso.js
@@ -1,6 +1,6 @@
 const sequelize = require("../db/connection");
 const { DataTypes } = require("sequelize");
-const { Person } = require("./persona");
+const Person  = require("./persona");
 
 const Course = sequelize.define("curso", {
     id_curso: {
