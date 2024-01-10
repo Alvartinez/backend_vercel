66389d4002767104e37efc4e38a72b6147765d69
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-10 00:26:40 -0500

    Update Module Router

--- src/routers/module.js
+++ src/routers/module.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getModule, newModule, getModules, getName } = require('../controllers/module');
+const { getModule, newModule, getModules, getName, updateModule } = require('../controllers/module');
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.get('/:id', getModule);
 router.post("/newModule", newModule);
 router.get("/getModules/:id_curso", getModules);
 router.get("/", getName);
+router.put("/updateModule", updateModule);
 
 module.exports = router;
