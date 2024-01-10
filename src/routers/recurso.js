f8197d9471a2eccddd7d443d8b248a7046e11647
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-10 17:05:43 -0500

    Update Resource Router

--- src/routers/recurso.js
+++ src/routers/recurso.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const {newResource, getResource} = require("../controllers/recurso");
+const {newResource, getResource, getAllResources} = require("../controllers/recurso");
 
 const router = Router();
 
+router.get("/", getAllResources);
 router.get("/:id", getResource);
 router.post("/newResource", newResource);
 
