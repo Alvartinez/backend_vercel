3597f580a6eb01a1516167182d41014d0e883a4c
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-10 00:19:11 -0500

    Update Module Router

--- src/routers/module.js
+++ src/routers/module.js
@@ -6,6 +6,6 @@ const router = Router();
 router.get('/:id', getModule);
 router.post("/newModule", newModule);
 router.get("/getModules/:id_curso", getModules);
-router.get("/getNameModule", getName);
+router.get("/", getName);
 
 module.exports = router;
