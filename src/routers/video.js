7505b2a6621293ebf593f26831c65ffdc50bfd48
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-11 12:05:35 -0500

    Update Video Router

--- src/routers/video.js
+++ src/routers/video.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { newVideo } = require("../controllers/video");
+const { editText } = require("../controllers/texto_plano");
 
 const router = Router();
 
 router.post("/newVideo", newVideo);
+router.put("/updateVideo", editText);
 
 module.exports = router;
