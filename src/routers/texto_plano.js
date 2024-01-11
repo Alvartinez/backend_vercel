b8128e6c08c45429e83654b97c9b752ec1cc4851
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-11 09:55:36 -0500

    Update Text Router

--- src/routers/texto_plano.js
+++ src/routers/texto_plano.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
-const { newText, editText } = require("../controllers/texto_plano");
+const { newText, editText, deleteText } = require("../controllers/texto_plano");
 
 const router = Router();
 
 router.post("/newText", newText);
 router.put("/updateText", editText);
-
+router.delete("/deleteText", deleteText);
 module.exports = router;
