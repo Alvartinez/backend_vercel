00c81cb0ce8d851cf1b68b17355e7edfa8e94115
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-12 18:46:31 -0500

    Update linea_tiempo Router

--- src/routers/linea_tiempo.js
+++ src/routers/linea_tiempo.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
-const { newLine, getLine, editLine, deleteLine } = require("../controllers/linea_tiempo");
+const { newLine, getLine, editLine, deleteLine, getLines } = require("../controllers/linea_tiempo");
 const { newHito, editHito, deleteHito, getHito, getHitos } = require("../controllers/hito");
 
 const router = Router();
 
 //Línea del tiempo
 router.get("/:id", getLine);
+router.get("/getLines", getLines);
 router.post("/newLine", newLine);
 router.put("/updateLine", editLine);
 router.delete("/deleteLine", deleteLine); 
