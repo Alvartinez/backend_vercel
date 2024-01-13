6e1285136fe688038ca547e9568a3978d9aa7d62
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-13 09:10:04 -0500

    Update Podcast Router

--- src/routers/podcast.js
+++ src/routers/podcast.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { newPodcast, editPodcast, getPodcast, deletePodcast } = require("../controllers/podcast");
+const { newPodcast, editPodcast, getPodcast, deletePodcast, getPodcasts } = require("../controllers/podcast");
 
 const router = Router();
 
 router.get("/:id", getPodcast);
+router.get("/podcasts/:id", getPodcasts);
 router.post("/newPodcast", newPodcast);
 router.put("/updatePodcast", editPodcast);
 router.delete("/deletePodcast", deletePodcast);
