cf8112959f494fbb24190aa4505ef51127cd8262
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-13 00:00:37 -0500

    Update Sabias Controller

--- src/controllers/sabias_que.js
+++ src/controllers/sabias_que.js
@@ -29,6 +29,76 @@ exports.newSabias = async (req, res) => {
             id_sabias_que: sabias.id_sabias_que
         });
 
+        res.status(200).json({
+            msg: "Sabías qué creado exitosamente"
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ msg: 'Se ha ocurrido un error' });
+    }
+
+}
+
+exports.editSabias = async (req, res) => {
+
+    const { sabia } = req.body;
+
+    try{
+
+        const {
+            id_sabias_que, 
+            titulo, 
+            enunciado, 
+            imagen, 
+            archivo
+        } = sabia;
+
+        const sabiaExiste = await sabiasQue.findOne({
+            where: {
+                id_sabias_que
+            }
+        });
+
+        if(!sabiaExiste){
+            return res.status(400).json({
+                msg: "No se ha encontrado el Sabías qué"
+            });
+        }
+
+        let cambiosRealizados = false;
+
+        if (sabiaExiste.titulo !== titulo) {
+            sabiaExiste.titulo = titulo;
+            cambiosRealizados = true;
+        }
+
+        if (sabiaExiste.enunciado !== enunciado) {
+            sabiaExiste.enunciado = enunciado;
+            cambiosRealizados = true;
+        }
+
+        if (sabiaExiste.imagen !== imagen) {
+            sabiaExiste.imagen = imagen;
+            cambiosRealizados = true;
+        }
+
+        if (sabiaExiste.archivo !== archivo) {
+            sabiaExiste.archivo = archivo;
+            cambiosRealizados = true;
+        }
+
+        if (cambiosRealizados) {
+            await sabiaExiste.save();
+            res.status(200).json({
+                msg: "Sabías qué actualizado exitosamente"
+            });
+        } else {
+            res.status(200).json({
+                msg: "No se realizaron cambios en el Sabías qué"
+            });
+        }
+
     } catch (error) {
         console.error(error);
         res.status(400).json({ msg: 'Se ha ocurrido un error' });
