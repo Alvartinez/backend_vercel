13a614e994ec2bac93752ac30d935fcdd78d1c44
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-13 00:49:36 -0500

    Update Sabias Router

--- src/routers/sabias_que.js
+++ src/routers/sabias_que.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
-const { newSabias, editSabias, deleteSabias } = require("../controllers/sabias_que");
+const { newSabias, editSabias, deleteSabias, getSabias } = require("../controllers/sabias_que");
 
 const router = Router();
 
-
+router.get("/:id", getSabias);
 router.post("/newSabia", newSabias);
 router.put("/updateSabia", editSabias);
 router.delete("/deleteSabia", deleteSabias);
