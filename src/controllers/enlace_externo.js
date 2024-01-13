140c7401c2d7b56f55b895c5d6cfe4328948d9a6
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-13 12:54:43 -0500

    Update Enlace Controller

--- src/controllers/enlace_externo.js
+++ src/controllers/enlace_externo.js
@@ -58,3 +58,57 @@ exports.newEnlace = async (req, res) => {
     }
 
 }
+
+exports.editEnlace = async (req, res) => {
+
+    const {enlace} = req.body;
+
+    try{
+
+        const {
+            id_enlace_externo, 
+            titulo, 
+            link
+        } = enlace;
+
+        const enlaceExiste = await Enlace.findOne({
+            where: {
+                id_enlace_externo
+            }
+        });
+
+        if(!enlaceExiste){
+            return res.status(400).json({
+                msg: "No se ha encontrado el enlace externo"
+            });
+        }
+
+        let cambiosRealizados = false;
+
+        if(enlaceExiste.titulo !== titulo) {
+            enlaceExiste.titulo = titulo;
+            cambiosRealizados = true;
+        }
+
+        if(enlaceExiste.enlace !== link) {
+            enlaceExiste.enlace = link;
+            cambiosRealizados = true;
+        }
+
+        if (cambiosRealizados) {
+            await enlaceExiste.save();
+            res.status(200).json({
+                msg: "Enlace externo actualizado exitosamente"
+            });
+        } else {
+            res.status(200).json({
+                msg: "No se realizaron cambios en el Enlace externo"
+            });
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ msg: 'Se ha ocurrido un error' });
+    }
+    
+}
