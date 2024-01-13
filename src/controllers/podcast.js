74846ae0bfce79bb81c40ead27fe2e483777d94e
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-13 09:08:55 -0500

    Update Podcast Controller

--- src/controllers/podcast.js
+++ src/controllers/podcast.js
@@ -150,6 +150,51 @@ exports.getPodcast = async (req, res) =>{
 
 }
 
+exports.getPodcasts = async (req, res) => {
+
+    const id = req.params.id;
+
+    try{
+
+        const moduloRecursos = await moduloRecurso.findAll({
+            where: {
+                id_modulo: id
+            }
+        });
+
+        const idRecursos = moduloRecursos.map(moduloRecurso => moduloRecurso.id_recurso);
+
+        const podcastRecursos = await recursoPodcast.findAll({
+            where: {
+                id_recurso: idRecursos
+            }
+        });
+
+        const idPodcast = podcastRecursos.map(podRecurso => podRecurso.id_sabias_que);
+
+        const pod = await Podcast.findAll({
+            where: {
+                id_podcast: idPodcast
+            }
+        });
+
+        if(!pod){
+            return res.status(400).json({
+                msg: "No se ha encontrado podcasts"
+            });
+        }
+
+        res.status(200).json({
+            Podcast: pod
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ msg: 'Se ha ocurrido un error' });
+    }
+
+}
+
 exports.deletePodcast = async (req, res) => {
 
     const { podcast } = req.body;
