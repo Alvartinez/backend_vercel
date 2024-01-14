f0e313b982f2712d2cf0b7e614c2a5c2c8d867b7
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-14 00:19:16 -0500

    Update Enlace Router

--- src/routers/enlace_externo.js
+++ src/routers/enlace_externo.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { newEnlace, editEnlace, deleteEnlace } = require("../controllers/enlace_externo");
+const { newEnlace, editEnlace, deleteEnlace, getEnlace, getEnlaceAll } = require("../controllers/enlace_externo");
 
 const router = Router();
 
+router.get("/:id", getEnlace);
+router.get("/enlaces/:id", getEnlaceAll);
 router.post("/newEnlace", newEnlace);
 router.put("/updateEnlace", editEnlace);
 router.delete("/deleteEnlace", deleteEnlace);
