6a039dca0fef6fd0eca8fd70b705ef684b56bb43
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-01-22 14:54:10 -0500

    Update Module Controller

--- src/controllers/module.js
+++ src/controllers/module.js
@@ -173,7 +173,7 @@ const getModule = async (req, res) => {
             include: [
                 {
                     model: Recurso,
-                    as: "recurso",
+                    as: "Recurso",
                     attributes: ['id_recurso', 'nombre'] 
                 }
             ]
