48677c7e56a148f8e5e937fe5a14c8c98e013cfe
Alvartinez <alvaro.martinezudes2@gmail.com>
2024-02-23 20:22:16 -0500

    Temporary Update Connection Local

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,24 +1,24 @@
 const { Sequelize } = require("sequelize");
 const { pg } = require("pg");
 
-// const sequelize = new Sequelize(process.env.DB_NAME || "healthTrain", process.env.DB_USER || "postgres", process.env.DB_PASSWORD || "310873292As", {
-//     host: "localhost",
-//     port: 5432,
-//     dialect: "postgres",
-//     dialectModule: pg
-// });
-
-const sequelize = new Sequelize(process.env.DB_NAME || "verceldb", process.env.DB_USER || "default", process.env.DB_PASSWORD || "5BWgNImv0iOw", {
-    host: "ep-raspy-water-a4kg3bn4-pooler.us-east-1.aws.neon.tech",
+const sequelize = new Sequelize(process.env.DB_NAME || "osl-udes", process.env.DB_USER || "postgres", process.env.DB_PASSWORD || "310873292As", {
+    host: "localhost",
     port: 5432,
     dialect: "postgres",
-    dialectModule: pg,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false // Esto deshabilita la verificación del certificado, asegúrate de entender las implicaciones de seguridad.
-        }
-    }
+    dialectModule: pg
 });
+
+// const sequelize = new Sequelize(process.env.DB_NAME || "verceldb", process.env.DB_USER || "default", process.env.DB_PASSWORD || "5BWgNImv0iOw", {
+//     host: "ep-raspy-water-a4kg3bn4-pooler.us-east-1.aws.neon.tech",
+//     port: 5432,
+//     dialect: "postgres",
+//     dialectModule: pg,
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false // Esto deshabilita la verificación del certificado, asegúrate de entender las implicaciones de seguridad.
+//         }
+//     }
+// });
 
 module.exports = sequelize;
